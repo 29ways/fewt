68cdbfdd1c2800295c9e9889e400f993f8d9e0e1
Adam Phillips <aphillips@scholastic.co.uk>
2019-05-12 22:26:26 +0100

    Improve display further using Listr
    This provides a better implementation of the UI and also supports
    concurrent requests.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import {Command, flags} from '@oclif/command'
 import cli from 'cli-ux'
+const Listr = require('listr')
+const EventEmitter = require('events');
 
 import ResourceExtracter from './resource-extracter'
 import Summariser, {Rule} from './summariser'
@@ -46,29 +48,36 @@ class Fewt extends Command {
   async run() {
     const {flags} = this.parse(Fewt)
 
-    const results = []
+    const results: any = []
 
-    for (let url of flags.url) {
-      cli.action.start(`Analysing ${url}`)
+    const tasks = []
 
-      const tracker = new Tracker(url)
-      await tracker.trace()
+    for (const [index, url] of flags.url.entries()) {
+      tasks.push({
+        title: url,
+        task: async () => {
+          const tracker = new Tracker(url)
+          await tracker.trace()
 
-      const extracter = new ResourceExtracter(tracker.traceAsJson())
-      const summary = new Summariser(Fewt.rules).run(extracter.resources())
+          const extracter = new ResourceExtracter(tracker.traceAsJson())
+          const summary = new Summariser(Fewt.rules).run(extracter.resources())
 
-      const tableData: { [key: string]: any } = {
-        url
-      }
+          const tableData: { [key: string]: any } = {
+            url
+          }
 
-      Fewt.rules.forEach(rule => {
-        tableData[rule.key] = (summary[rule.key].totalEncodedDataLength / 1024).toFixed(2)
-        tableData[`${rule.key}-length`] = summary[rule.key].resources.length
+          Fewt.rules.forEach(rule => {
+            tableData[rule.key] = (summary[rule.key].totalEncodedDataLength / 1024).toFixed(2)
+            tableData[`${rule.key}-length`] = summary[rule.key].resources.length
+          })
+
+          results[index] = tableData
+        }
       })
-
-      results.push(tableData)
-      cli.action.stop()
     }
+
+    new EventEmitter().setMaxListeners(tasks.length)
+    await new Listr(tasks, {concurrent: true}).run()
 
     const columns: { [key: string]: any} = {url: {}}
     Fewt.rules.forEach(rule => {
