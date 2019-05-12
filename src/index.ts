55ea3da6f11d1ab777e998209bdc4ed7598878da
Adam Phillips <aphillips@scholastic.co.uk>
2019-05-12 21:07:54 +0100

    Support multiple URLs in a single analysis
    This allows multiple URLs to be passed and the results combined into a
    single table.
    
    This did run into an issue to oclif whereby passing an arbitrary number
    of arguments doesn't seem to work correctly. It does support multiple
    arguments for a flag though so this will suffice for the moment. Beware
    that this behaviour may change as there is a Github issue open as it has
    caused some other problems.
    
    `https://github.com/oclif/oclif/issues/190`

--- src/index.ts
+++ src/index.ts
@@ -12,60 +12,71 @@ class Fewt extends Command {
     // add --version flag to show CLI version
     version: flags.version({char: 'v'}),
     help: flags.help({char: 'h'}),
+    url: flags.string({
+      char: 'u',
+      multiple: true
+    })
   }
 
-  static args = [{name: 'url'}]
+  static args = []
+
+  static rules: Rule[] = [
+    {
+      key: 'html',
+      match: resource => resource.response.mimeType === 'text/html'
+    },
+    {
+      key: 'css',
+      match: resource => resource.response.mimeType === 'text/css'
+    },
+    {
+      key: 'js',
+      match: resource => resource.response.mimeType.match(/javascript$/) !== null
+    },
+    {
+      key: 'img',
+      match: resource => resource.response.mimeType.match(/^image/) !== null
+    },
+    {
+      key: 'fonts',
+      match: resource => resource.response.mimeType.match(/font/) !== null
+    }
+  ]
 
   async run() {
-    const {args} = this.parse(Fewt)
-    cli.action.start(`Analysing ${args.url}`)
+    const {flags} = this.parse(Fewt)
 
-    const tracker = new Tracker(args.url)
-    await tracker.trace()
+    const results = []
 
-    const extracter = new ResourceExtracter(tracker.traceAsJson())
-    let rules: Rule[] = [
-      {
-        key: 'html',
-        match: resource => resource.response.mimeType === 'text/html'
-      },
-      {
-        key: 'css',
-        match: resource => resource.response.mimeType === 'text/css'
-      },
-      {
-        key: 'js',
-        match: resource => resource.response.mimeType.match(/javascript$/) !== null
-      },
-      {
-        key: 'img',
-        match: resource => resource.response.mimeType.match(/^image/) !== null
-      },
-      {
-        key: 'fonts',
-        match: resource => resource.response.mimeType.match(/font/) !== null
+    for (let url of flags.url) {
+      cli.action.start(`Analysing ${url}`)
+
+      const tracker = new Tracker(url)
+      await tracker.trace()
+
+      const extracter = new ResourceExtracter(tracker.traceAsJson())
+      const summary = new Summariser(Fewt.rules).run(extracter.resources())
+
+      const tableData: { [key: string]: any } = {
+        url
       }
-    ]
 
-    const summary = new Summariser(rules).run(extracter.resources())
+      Fewt.rules.forEach(rule => {
+        tableData[rule.key] = (summary[rule.key].totalEncodedDataLength / 1024).toFixed(2)
+        tableData[`${rule.key}-length`] = summary[rule.key].resources.length
+      })
 
-    const tableData: { [key: string]: any } = {
-      url: args.url
+      results.push(tableData)
+      cli.action.stop()
     }
 
-    rules.forEach(rule => {
-      tableData[rule.key] = (summary[rule.key].totalEncodedDataLength / 1024).toFixed(2)
-      tableData[`${rule.key}-length`] = summary[rule.key].resources.length
-    })
-
     const columns: { [key: string]: any} = {url: {}}
-    rules.forEach(rule => {
+    Fewt.rules.forEach(rule => {
       columns[rule.key] = {}
       columns[`${rule.key}-length`] = {}
     })
 
-    cli.action.stop()
-    cli.table([tableData], columns)
+    cli.table(results, columns)
   }
 }
 
